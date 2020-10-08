4ca63ea742e73e02853d8d22b98ec5f1b393025d
yuuuko86326274 <yuko.67.53@gmail.com>
2020-10-08 19:09:16 +0900

    Update unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -5,7 +5,7 @@ app_path = File.expand_path('../../../', __FILE__)
 worker_processes 1
 
 #アプリケーションの設置されているディレクトリを指定
-working_directory "{app_path}/current"
+working_directory "#{app_path}/current"
 
 #Unicornの起動に必要なファイルの設置場所を指定
 pid "#{app_path}/shared/tmp/pids/unicorn.pid"
