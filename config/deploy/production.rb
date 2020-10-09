2b7f8f0fbaf1c2d2e288b441fe8c8d2f4dab2100
yuuuko86326274 <yuko.67.53@gmail.com>
2020-10-09 17:16:36 +0900

    Update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,4 +59,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server '54.65.96.96', user: 'ec2-user', roles: %w{app db web}
+server '54.150.31.80', user: 'ec2-user', roles: %w{app db web}
