53d86e2a7bc83585d261d7439628ec778fd8c909
yuuuko86326274 <yuko.67.53@gmail.com>
2020-11-07 23:29:44 +0900

    basic認証削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,4 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth
-
-  private
-
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
-    end
-  end
 
   def after_sign_in_path_for(_resource)
     root_path
