b1042b9bbc8b5f8dc66c652bd2d2b9f1aee4e1d7
yuuuko86326274 <yuko.67.53@gmail.com>
2020-11-27 00:03:49 +0900

    rspec修正

--- spec/factories/traders.rb
+++ spec/factories/traders.rb
@@ -4,12 +4,12 @@ FactoryBot.define do
     password = Faker::Internet.password(min_length: 6)
     password {password}
     password_confirmation {password}
-    t_name {'（株）ニャンコ保護'}
+    t_name { '株式会社ニャンコ保護' }
     t_postal_code { '111-1111' }
     area_id { Faker::Number.between(from: 1, to: 47)}
-    t_city {'横浜市'}
-    t_address_num {'下島町5丁目3ー３３'}
-    t_building_num {'ビル303'}
+    t_city { '横浜市' }
+    t_address_num { '下島町5丁目3ー３３' }
+    t_building_num { 'ビル303' }
     t_tel { Faker::Number.number(digits: 11)}
   end
 end
