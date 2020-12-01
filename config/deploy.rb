c370576c214778776662213df3e7f4a2ade49084
yuuuko86326274 <yuko.67.53@gmail.com>
2020-12-01 09:11:58 +0900

    cercleCD

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,6 +13,21 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 set :rbenv_type, :user
 set :rbenv_ruby, '2.6.5'
 
+# 出力するログのレベル。
+set :log_level, :debug
+
+# deploy先のディレクトリ。
+set :deploy_to, '/var/www/cat-28730'
+
+# secret_base_keyを読み込ませるため追記
+set :linked_files, %w[config/master.key]
+
+# シンボリックリンクをはるファイル。
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/settings.yml', '.env')
+
+# シンボリックリンクをはるフォルダ。
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
                                   keys: ['~/.ssh/Yuko1208.pem'] 
@@ -24,10 +39,39 @@ set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
-# デプロイ処理が終わった後、Unicornを再起動するための記述
-after 'deploy:publishing', 'deploy:restart'
+
 namespace :deploy do
+  desc 'Restart application'
   task :restart do
     invoke 'unicorn:restart'
+  end
+
+  desc 'Create database'
+  task :db_create do
+    on roles(:db) do |_host|
+      with rails_env: fetch(:rails_env) do
+        within current_path do
+          execute :bundle, :exec, :rake, 'db:create'
+        end
+      end
+    end
+  end
+
+  desc 'Run seed'
+  task :seed do
+    on roles(:app) do
+      with rails_env: fetch(:rails_env) do
+        within current_path do
+          execute :bundle, :exec, :rake, 'db:seed'
+        end
+      end
+    end
+  end
+
+  after :publishing, :restart
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+    end
   end
 end
