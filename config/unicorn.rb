014ac7ba32152f1cbf1249d409cfeeb9677bc4a0
yuuuko86326274 <yuko.67.53@gmail.com>
2021-01-23 21:11:54 +0900

    unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,7 +1,7 @@
 $worker  = 2
   $timeout = 30
   $app_dir = "/var/www/workdir" 
-  $listen  = File.expand_path 'tmp/sockets/.unicorn.sock', $app_dir
+  $listen  = File.expand_path 'tmp/sockets/unicorn.sock', $app_dir
   $pid     = File.expand_path 'tmp/pids/unicorn.pid', $app_dir
   $std_log = File.expand_path 'log/unicorn.log', $app_dir
   # set config
