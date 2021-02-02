e4abd63b766a7df82f7c8f3c8ffb1f49defa9840
yuuuko86326274 <yuko.67.53@gmail.com>
2021-02-03 00:21:49 +0900

    nginx

--- config/deploy.rb
+++ config/deploy.rb
@@ -33,10 +33,10 @@ set :ssh_options, auth_methods: ['publickey'],
                                   keys: ['~/.ssh/Yuko1208.pem'] 
 
 # プロセス番号を記載したファイルの場所
-set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+set :unicorn_pid, -> { "tmp/pids/unicorn.pid" }
 
 # Unicornの設定ファイルの場所
-set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :unicorn_config_path, -> { "config/unicorn.rb" }
 set :keep_releases, 5cd
 
 
