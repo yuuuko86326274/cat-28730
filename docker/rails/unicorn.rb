b652b894ae50c10ae20ae6bec41fc3429b8deeec
yuuuko86326274 <yuko.67.53@gmail.com>
2021-02-13 16:55:08 +0900

    docker

--- docker/rails/unicorn.rb
+++ docker/rails/unicorn.rb
@@ -1,87 +1,57 @@
-$worker  = 2
-  $timeout = 30
-  $app_dir = "/var/www/workdir" 
-  $listen  = File.expand_path 'tmp/sockets/unicorn.sock', $app_dir
-  $pid     = File.expand_path 'tmp/pids/unicorn.pid', $app_dir
-  $std_log = File.expand_path 'log/unicorn.log', $app_dir
-  # set config
-  worker_processes  $worker
-  working_directory $app_dir
-  stderr_path $std_log
-  stdout_path $std_log
-  timeout $timeout
-  listen  $listen
-  pid $pid
-  # loading booster
-  preload_app true
-  # before starting processes
-  before_fork do |server, worker|
-    defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
-    old_pid = "#{server.config[:pid]}.oldbin"
-    if old_pid != server.pid
-      begin
-        Process.kill "QUIT", File.read(old_pid).to_i
-      rescue Errno::ENOENT, Errno::ESRCH
-      end
+# Railsのルートパスを求める。(RAILS_ROOT/config/unicorn.rbに配置している場合。)
+rails_root = File.expand_path('../../', __FILE__)
+# RAILS_ENVを求める。（RAILS_ENV毎に挙動を変更したい場合に使用。今回は使用しません。)
+# rails_env = ENV['RAILS_ENV'] || "development"
+
+# 追記に記載してます。入れた方がいいです。
+ENV['BUNDLE_GEMFILE'] = rails_root + "/Gemfile"
+
+# Unicornは複数のワーカーで起動するのでワーカー数を定義
+# サーバーのメモリなどによって変更すること。
+worker_processes 2
+
+# 指定しなくても良い。
+# Unicornの起動コマンドを実行するディレクトリを指定します。
+# （記載しておけば他のディレクトリでこのファイルを叩けなくなる。）
+working_directory rails_root
+
+# 接続タイムアウト時間
+timeout 30
+
+# Unicornのエラーログと通常ログの位置を指定。
+stderr_path File.expand_path('../../log/unicorn_stderr.log', __FILE__)
+stdout_path File.expand_path('../../log/unicorn_stdout.log', __FILE__)
+
+# Nginxで使用する場合は以下の設定を行う。
+listen File.expand_path('../../tmp/sockets/unicorn.sock', __FILE__)
+# とりあえず起動して動作確認をしたい場合は以下の設定を行う。
+# listen 80
+# ※「backlog」や「tcp_nopush」の設定もあるけど、よくわかって無い。
+
+# プロセスの停止などに必要なPIDファイルの保存先を指定。
+pid File.expand_path('../../tmp/pids/unicorn.pid', __FILE__)
+
+# 基本的には`true`を指定する。Unicornの再起動時にダウンタイムなしで再起動が行われる。
+preload_app true
+# 効果なしとの記事を見たので、コメントアウト。
+# GC.respond_to?(:copy_on_write_friendly=) and GC.copy_on_write_friendly = true
+
+# USR2シグナルを受けると古いプロセスを止める。
+# 後述するが、記述しておくとNginxと連携する時に良いことがある。
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) and
+      ActiveRecord::Base.connection.disconnect!
+
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if old_pid != server.pid
+    begin
+      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+      Process.kill(sig, File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
     end
   end
-  # after finishing processes
-  after_fork do |server, worker|
-    defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
-  end
+end
 
-# # サーバ上でのアプリケーションコードが設置されているディレクトリを変数に入れておく
-# app_path = File.expand_path('../..', __FILE__)
-
-# #アプリケーションサーバの性能を決定する
-# worker_processes 1
-
-# #アプリケーションの設置されているディレクトリを指定
-# working_directory app_path
-
-# #Unicornの起動に必要なファイルの設置場所を指定
-# pid "#{app_path}/tmp/pids/unicorn.pid"
-
-# #ポート番号を指定
-# listen "#{app_path}/tmp/sockets/unicorn.sock"
-
-# #エラーのログを記録するファイルを指定
-# stderr_path "#{app_path}/log/unicorn.log"
-
-# #通常のログを記録するファイルを指定
-# stdout_path "#{app_path}/log/unicorn.log"
-
-# #Railsアプリケーションの応答を待つ上限時間を設定
-# timeout 60
-
-# #以下は応用的な設定なので説明は割愛
-
-# preload_app true
-# GC.respond_to?(:copy_on_write_friendly=) && GC.copy_on_write_friendly = true
-
-# check_client_connection false
-
-# run_once = true
-
-# before_fork do |server, worker|
-#   defined?(ActiveRecord::Base) &&
-#     ActiveRecord::Base.connection.disconnect!
-
-#   if run_once
-#     run_once = false # prevent from firing again
-#   end
-
-#   old_pid = "#{server.config[:pid]}.oldbin"
-#   if File.exist?(old_pid) && server.pid != old_pid
-#     begin
-#       sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
-#       Process.kill(sig, File.read(old_pid).to_i)
-#     rescue Errno::ENOENT, Errno::ESRCH => e
-#       logger.error e
-#     end
-#   end
-# end
-
-# after_fork do |_server, _worker|
-#   defined?(ActiveRecord::Base) && ActiveRecord::Base.establish_connection
-# end
+after_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+end
