2a7f2f9b7ffdf31b0b7e30bb6abe0253ce486452
yuuuko86326274 <yuko.67.53@gmail.com>
2021-02-16 19:22:32 +0900

    compose

--- docker/rails/unicorn.rb
+++ docker/rails/unicorn.rb
@@ -55,3 +55,43 @@ end
 after_fork do |server, worker|
   defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
 end
+
+# $worker  = 2
+#   $timeout = 30
+#   $app_dir = "/workdir" #自分のアプリケーションまでのpath
+#   $listen  = File.expand_path 'tmp/sockets/unicorn.sock', $app_dir
+#   $pid     = File.expand_path 'tmp/pids/unicorn.pid', $app_dir
+#   $std_log = File.expand_path 'log/.unicorn.log', $app_dir
+#   # set config
+#   worker_processes  $worker
+#   working_directory $app_dir
+#   stderr_path $std_log
+#   stdout_path $std_log
+#   timeout $timeout
+#   listen  $listen
+#   pid $pid
+#   # loading booster
+#   preload_app true
+#   # before starting processes
+#   before_fork do |server, worker|
+#     defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
+#     old_pid = "#{server.config[:pid]}.oldbin"
+#     if old_pid != server.pid
+#       begin
+#         Process.kill "QUIT", File.read(old_pid).to_i
+#       rescue Errno::ENOENT, Errno::ESRCH
+#       end
+#     end
+#   end
+#   # after finishing processes
+#   after_fork do |server, worker|
+#     defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+#   end
+
+# worker_processes 2
+
+# pid "/var/run/unicorn.pid"
+# listen "/var/tmp/unicorn.sock"
+
+# stdout_path "./log/unicorn.stdout.log"
+# stderr_path "./log/unicorn.stderr.log"
