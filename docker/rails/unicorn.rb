4014147d37d3fe00fbc131983123b112ed538eb7
yuuuko86326274 <yuko.67.53@gmail.com>
2021-03-01 20:05:58 +0900

    Update unicorn.rb

--- docker/rails/unicorn.rb
+++ docker/rails/unicorn.rb
@@ -1,3 +1,65 @@
+# # Where our application lives. $RAILS_ROOT is defined in our Dockerfile.
+# app_path = "/workdir"
+ 
+# # Set the server's working directory
+# working_directory app_path
+ 
+# # Define where Unicorn should write its PID file
+# pid "#{app_path}/tmp/pids/unicorn.pid"
+ 
+# # Bind Unicorn to the container's default route, at port 3000
+# listen "0.0.0.0:3000"
+ 
+# # Define where Unicorn should write its log files
+# stderr_path "#{app_path}/log/unicorn.stderr.log"
+# stdout_path "#{app_path}/log/unicorn.stdout.log"
+ 
+# # Define the number of workers Unicorn should spin up.
+# # A new Rails app just needs one. You would scale this 
+# # higher in the future once your app starts getting traffic.
+# # See https://unicorn.bogomips.org/TUNING.html
+# worker_processes 1
+ 
+# # Make sure we use the correct Gemfile on restarts
+# before_exec do |server|
+#   ENV['BUNDLE_GEMFILE'] = "#{app_path}/Gemfile"
+# end
+ 
+# # Speeds up your workers.
+# # See https://unicorn.bogomips.org/TUNING.html
+# preload_app true
+ 
+# #
+# # Below we define how our workers should be spun up. 
+# # See https://unicorn.bogomips.org/Unicorn/Configurator.html
+# #
+ 
+# before_fork do |server, worker|
+#   # the following is highly recomended for Rails + "preload_app true"
+#   # as there's no need for the master process to hold a connection
+#   if defined?(ActiveRecord::Base)
+#     ActiveRecord::Base.connection.disconnect!
+#   end
+ 
+#   # Before forking, kill the master process that belongs to the .oldbin PID.
+#   # This enables 0 downtime deploys.
+#   old_pid = "#{server.config[:pid]}.oldbin"
+#   if File.exists?(old_pid) && server.pid != old_pid
+#     begin
+#       Process.kill("QUIT", File.read(old_pid).to_i)
+#     rescue Errno::ENOENT, Errno::ESRCH
+#       # someone else did our job for us
+#     end
+#   end
+# end
+ 
+# after_fork do |server, worker|
+#   if defined?(ActiveRecord::Base)
+#     ActiveRecord::Base.establish_connection
+#   end
+# end
+
+
 # Railsのルートパスを求める。(RAILS_ROOT/config/unicorn.rbに配置している場合。)
 rails_root = ENV['RAILS_ROOT'] #File.expand_path('../../', __FILE__)
 # RAILS_ENVを求める。（RAILS_ENV毎に挙動を変更したい場合に使用。今回は使用しません。)
@@ -47,11 +109,52 @@ before_fork do |server, worker|
     begin
       sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
       Process.kill(sig, File.read(old_pid).to_i)
-    rescue Errno::ENOENT, Errno::ESRCH
+    rescue Errno::ENOENT, Errno::ESRCH => e
+      logger.error e
     end
   end
 end
 
-after_fork do |server, worker|
+after_fork do |_server, _worker|
   defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
 end
+
+# $worker  = 2
+#   $timeout = 30
+#   $app_dir = "/workdir" #自分のアプリケーションまでのpath
+#   $listen  = File.expand_path 'tmp/sockets/unicorn.sock', $app_dir
+#   $pid     = File.expand_path 'tmp/pids/unicorn.pid', $app_dir
+#   $std_log = File.expand_path 'log/.unicorn.log', $app_dir
+#   # set config
+#   worker_processes  $worker
+#   working_directory $app_dir
+#   stderr_path $std_log
+#   stdout_path $std_log
+#   timeout $timeout
+#   listen  $listen
+#   pid $pid
+#   # loading booster
+#   preload_app true
+#   # before starting processes
+#   before_fork do |server, worker|
+#     defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
+#     old_pid = "#{server.config[:pid]}.oldbin"
+#     if old_pid != server.pid
+#       begin
+#         Process.kill "QUIT", File.read(old_pid).to_i
+#       rescue Errno::ENOENT, Errno::ESRCH
+#       end
+#     end
+#   end
+#   # after finishing processes
+#   after_fork do |server, worker|
+#     defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+#   end
+
+# worker_processes 1
+
+# pid "/var/run/unicorn.pid"
+# listen "/var/tmp/unicorn.sock"
+
+# stdout_path "./log/unicorn.stdout.log"
+# stderr_path "./log/unicorn.stderr.log"
