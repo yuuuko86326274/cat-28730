756db84954eab393f7595213f41a33bdd7b0a293
yuuuko86326274 <yuko.67.53@gmail.com>
2021-03-01 23:55:12 +0900

    cercleci

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -109,13 +109,12 @@ before_fork do |server, worker|
     begin
       sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
       Process.kill(sig, File.read(old_pid).to_i)
-    rescue Errno::ENOENT, Errno::ESRCH => e
-      logger.error e
+    rescue Errno::ENOENT, Errno::ESRCH
     end
   end
 end
 
-after_fork do |_server, _worker|
+after_fork do |server, worker|
   defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
 end
 
