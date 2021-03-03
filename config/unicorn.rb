350065bbf6b99ba768d88ca241f6ddf342162b30
yuuuko86326274 <yuko.67.53@gmail.com>
2021-03-03 21:20:55 +0900

    var/www

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -61,7 +61,7 @@
 
 
 # Railsのルートパスを求める。(RAILS_ROOT/config/unicorn.rbに配置している場合。)
-rails_root = "/home/ec2-user/workdir" #File.expand_path('../../', __FILE__)   #ENV['RAILS_ROOT']
+rails_root = "/var/www/workdir" #File.expand_path('../../', __FILE__)   #ENV['RAILS_ROOT']
 # RAILS_ENVを求める。（RAILS_ENV毎に挙動を変更したい場合に使用。今回は使用しません。)
 # rails_env = ENV['RAILS_ENV'] || "development"
 
