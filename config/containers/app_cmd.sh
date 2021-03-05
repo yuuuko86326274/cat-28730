cdca5df7650890ffa1761c24de3bfca9cbfad17d
yuuuko86326274 <yuko.67.53@gmail.com>
2021-03-05 13:43:30 +0900

    cap外す

--- config/containers/app_cmd.sh
+++ config/containers/app_cmd.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
  
 # Prefix `bundle` with `exec` so unicorn shuts down gracefully on SIGTERM (i.e. `docker stop`)
-exec bundle exec unicorn -c /var/www/workdir/config/unicorn.rb -E $RAILS_ENV -D;
+bundle exec unicorn -c /var/www/workdir/config/unicorn.rb -E $RAILS_ENV -D;
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,77 +1,77 @@
-# capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
-lock '3.14.1'
+# # capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
+# lock '3.14.1'
 
-# Capistranoのログの表示に利用する
-set :application, 'cat-28730'
+# # Capistranoのログの表示に利用する
+# set :application, 'cat-28730'
 
-# どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:yuuuko86326274/cat-28730.git'
+# # どのリポジトリからアプリをpullするかを指定する
+# set :repo_url,  'git@github.com:yuuuko86326274/cat-28730.git'
 
-# バージョンが変わっても共通で参照するディレクトリを指定
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+# # バージョンが変わっても共通で参照するディレクトリを指定
+# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
-set :rbenv_type, :user
-set :rbenv_ruby, '2.6.5'
-# set :rbenv_path, '/home/ec2-user/.rbenv/'
-# 出力するログのレベル。
-set :log_level, :debug
+# set :rbenv_type, :user
+# set :rbenv_ruby, '2.6.5'
+# # set :rbenv_path, '/home/ec2-user/.rbenv/'
+# # 出力するログのレベル。
+# set :log_level, :debug
 
-# deploy先のディレクトリ。
-set :deploy_to, '/var/www/workdir'
+# # deploy先のディレクトリ。
+# set :deploy_to, '/var/www/workdir'
 
-# secret_base_keyを読み込ませるため追記
-set :linked_files, fetch(:linked_files, []).push("config/master.key")
+# # secret_base_keyを読み込ませるため追記
+# set :linked_files, fetch(:linked_files, []).push("config/master.key")
 
-# シンボリックリンクをはるファイル。
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/settings.yml')
+# # シンボリックリンクをはるファイル。
+# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/settings.yml')
 
-# シンボリックリンクをはるフォルダ。
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+# # シンボリックリンクをはるフォルダ。
+# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
-# どの公開鍵を利用してデプロイするか
-set :ssh_options, auth_methods: ['publickey'],
-                                  keys: ['~/.ssh/Yuko1208.pem'] 
+# # どの公開鍵を利用してデプロイするか
+# set :ssh_options, auth_methods: ['publickey'],
+#                                   keys: ['~/.ssh/Yuko1208.pem'] 
 
-# プロセス番号を記載したファイルの場所
-set :unicorn_pid, -> { "tmp/pids/unicorn.pid" }
+# # プロセス番号を記載したファイルの場所
+# set :unicorn_pid, -> { "tmp/pids/unicorn.pid" }
 
-# Unicornの設定ファイルの場所
-set :unicorn_config_path, -> { "config/unicorn.rb" }
-set :keep_releases, 5cd
+# # Unicornの設定ファイルの場所
+# set :unicorn_config_path, -> { "config/unicorn.rb" }
+# set :keep_releases, 5cd
 
 
-namespace :deploy do
-  desc 'Restart application'
-  task :restart do
-    invoke 'unicorn:restart'
-  end
+# namespace :deploy do
+#   desc 'Restart application'
+#   task :restart do
+#     invoke 'unicorn:restart'
+#   end
 
-  desc 'Create database'
-  task :db_create do
-    on roles(:db) do |_host|
-      with rails_env: fetch(:rails_env) do
-        within current_path do
-          execute :bundle, :exec, :rake, 'db:create'
-        end
-      end
-    end
-  end
+#   desc 'Create database'
+#   task :db_create do
+#     on roles(:db) do |_host|
+#       with rails_env: fetch(:rails_env) do
+#         within current_path do
+#           execute :bundle, :exec, :rake, 'db:create'
+#         end
+#       end
+#     end
+#   end
 
-  desc 'Run seed'
-  task :seed do
-    on roles(:app) do
-      with rails_env: fetch(:rails_env) do
-        within current_path do
-          execute :bundle, :exec, :rake, 'db:seed'
-        end
-      end
-    end
-  end
+#   desc 'Run seed'
+#   task :seed do
+#     on roles(:app) do
+#       with rails_env: fetch(:rails_env) do
+#         within current_path do
+#           execute :bundle, :exec, :rake, 'db:seed'
+#         end
+#       end
+#     end
+#   end
 
-  after :publishing, :restart
+#   after :publishing, :restart
 
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-    end
-  end
-end
+#   after :restart, :clear_cache do
+#     on roles(:web), in: :groups, limit: 3, wait: 10 do
+#     end
+#   end
+# end
