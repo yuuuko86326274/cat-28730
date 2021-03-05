268b4e58613918d8f5b24e7581c3d1a7efe00b4a
yuuuko86326274 <yuko.67.53@gmail.com>
2021-03-05 11:53:48 +0900

    ecs

--- config/containers/app_cmd.sh
+++ config/containers/app_cmd.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
  
 # Prefix `bundle` with `exec` so unicorn shuts down gracefully on SIGTERM (i.e. `docker stop`)
-exec bundle exec unicorn -c config/unicorn.rb -E $RAILS_ENV -D;
+exec bundle exec unicorn -c /var/www/workdir/config/unicorn.rb -E $RAILS_ENV -D;
