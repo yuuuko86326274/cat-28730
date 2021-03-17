3097a7f159d909109a07e890bba9afd9055593d0
yuuuko86326274 <yuko.67.53@gmail.com>
2021-03-17 16:46:58 +0900

    readme

--- config/puma.rb
+++ config/puma.rb
@@ -1,7 +1,7 @@
 # アプリケーションディレクトリ
 app_dir = File.expand_path("../..", __FILE__)
 # ソケット通信を図る為bindでURI指定
-bind "unix://#{app_dir}/tmp/sockets/puma.sock"#切替
+# bind "unix://#{app_dir}/tmp/sockets/puma.sock"#切替
 # PIDファイル所在(プロセスID)
 pidfile "#{app_dir}/tmp/pids/puma.pid"
 # stateファイルはpumactlコマンドでサーバーを操作する。その所在。
@@ -10,5 +10,5 @@ state_path "#{app_dir}/tmp/pids/puma.state"
 stdout_redirect "#{app_dir}/log/puma.stdout.log", "#{app_dir}/log/puma.stderr.log", true
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 threads threads_count, threads_count
-# port       ENV.fetch("PORT") { 3000 }#切替
+port       ENV.fetch("PORT") { 3000 }#切替
 plugin :tmp_restart
