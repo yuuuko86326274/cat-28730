16da3aa957009dcdf77c30cc11f9d22f4608189d
yuuuko86326274 <yuko.67.53@gmail.com>
2021-03-18 02:12:37 +0900

    node.js読み込み修正

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,5 @@
 const pay = () => {
-  Payjp.setPublicKey("pk_test_9cc2eff2625d4b9434450061");
+  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
   const form = document.getElementById("charge-form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
