c4d4cb5292c4ebf2dd0b11575a41746e713d287e
yuuuko86326274 <yuko.67.53@gmail.com>
2021-05-17 23:50:41 +0900

    swiper

--- config/routes.rb
+++ config/routes.rb
@@ -22,8 +22,6 @@ Rails.application.routes.draw do
   # resources :users, only: [:new, :create, :destroy]
   get 'cats/fortune'
   get 'cats/info'
-  get 'cats/_top2'
-  get 'cats/_top1'
   resources :cats do
     resources :comments, only: :create
     collection do
